e8a77d324eab5e76881f1d36b279963e6d97d5be
talha-mahmood <talhamahmood1261@gmail.com>
2023-12-19 10:19:20 +0500

    10

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -27,7 +27,9 @@ const Header = () => {
         <div className="flex justify-between mx-auto py-0 mr-2 ml-2">
           <div className="mt-6 ">
             <Link to={'/'} className="flex items-center gap-1">
-             
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+              </svg>
               <span className="font-bold text-xl">TourMyPak</span>
             </Link>
           </div>
